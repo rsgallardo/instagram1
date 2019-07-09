755328d0d22e44aacca6599e01683805dcd02a44
Robert Gallardo <rgallardo@fb.com>
2019-07-09 12:56:12 -0700

    Initial Commit

---
+++ instagram1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  instagram1
+//
+//  Created by rgallardo on 7/9/19.
+//  Copyright © 2019 rgallardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ instagram1/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  instagram1
+//
+//  Created by rgallardo on 7/9/19.
+//  Copyright © 2019 rgallardo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
